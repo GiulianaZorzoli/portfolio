496dfc25992ec1dd1e8adfb3dd383d813becfc2a
Giuliana_Zorzoli <giulijazorzoli@gmail.com>
2024-02-02 12:22:15 -0300

    button change

--- src/assets/Skills.jsx
+++ src/assets/Skills.jsx
@@ -92,11 +92,11 @@ export const Skills = ({lang}) => {
                   </div>
                 </div>
               </div>
-              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <button className="carousel-control-prev carouselBTN" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
               </button>
-              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <button className="carousel-control-next carouselBTN" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
               </button>
